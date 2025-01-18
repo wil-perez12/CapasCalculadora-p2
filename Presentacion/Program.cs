cb93c943f22f4a9dcc9315a6e7d5f38ab22f7008
ing Wilnel <perezwilnel12@gmail.com>
2025-01-18 12:44:21 -0400

    validaciones para el menu y cambios en la presentacion de la consola v1.2 18/1/25

--- Presentacion/Program.cs
+++ Presentacion/Program.cs
@@ -17,7 +17,7 @@ do {
 
     opcion = int.Parse(Console.ReadLine());
 
-    if (opcion != 5 && opcion !=6)
+    if (opcion != 5 && opcion !=6 && opcion < 6)
     {
         Console.WriteLine("ingrese el primer numero");
         servicio.numero1 = int.Parse(Console.ReadLine());
@@ -25,37 +25,45 @@ do {
         Console.WriteLine("ingrese el segundo numero");
         servicio.numero2 = int.Parse(Console.ReadLine());
     }
-    
 
+   
+   
 
     switch (opcion)
     {
         case 1:
             servicio.sumar(servicio.numero1, servicio.numero2);
+            Console.WriteLine("---------------------------------------------------");
             break;
 
         case 2:
             servicio.restar(servicio.numero1, servicio.numero2);
+            Console.WriteLine("---------------------------------------------------");
             break;
 
         case 3:
             servicio.multiplicar(servicio.numero1, servicio.numero2);
+            Console.WriteLine("---------------------------------------------------");
             break;
         
         case 4:
             servicio.dividir(servicio.numero1, servicio.numero2);
+            Console.WriteLine("---------------------------------------------------");
             break;
 
         case 5:
             servicio.MostarHistorial();
+            Console.WriteLine("---------------------------------------------------");
             break;
 
         case 6:
             Console.WriteLine("Saliendoo....");
+            Console.WriteLine("---------------------------------------------------");
             break;
 
         default:
             Console.WriteLine("Opción invalida. Vuelva a intentar!");
+            Console.WriteLine("---------------------------------------------------");
             break;
     }
 
